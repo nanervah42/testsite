cdf41c9c0f90edd23b83b76c57faf2bc51568cb8
nanervah <nanervah@yandex.ru>
2021-10-25 02:08:53 +0700

    #12

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -11,5 +11,6 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello', arg2='world'):
+    # categories = Category.objects.all()
     categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {"categories": categories, "arg1": arg1, "arg2": arg2}
